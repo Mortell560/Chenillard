e04f30d5f9af36ea60a98f4ec1803dd53d8732fd
Mortell560 <mortell560@gmail.com>
2023-10-10 20:49:07 +0000

    printfs

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "mbed.h"
 #include "C12832.h"
+#include "mbed2/299/platform/wait_api.h"
+#include <algorithm>
 
 /* Inputs */
 DigitalIn U(p15);
@@ -17,6 +19,10 @@ DigitalOut led4(LED4);
 
 C12832 lcd(p5, p7, p6, p8, p11);
 DigitalOut leds[4]={led1, led2, led3, led4};
+char* dirs[2] = {"Left", "Right"};
+int curr_dir = 0;
+int val = 1;
+
 
 void ResetLeds(){
     // Eteint toutes les leds
@@ -33,8 +39,39 @@ void DisplayIntLeds(int j){
     }
 }
 
+void ShiftRight(){
+
+}
+
+void showInfoInputs(){
+    lcd.locate(0,lcd.height()-4);
+    lcd.printf("%i", U.read());
+
+    lcd.locate(lcd.width()/5, lcd.height()-4);
+    lcd.printf("%i", D.read());
+
+    lcd.locate(lcd.width()/5*2, lcd.height()-4);
+    lcd.printf("%i", L.read());
+
+    lcd.locate(lcd.width()/5*3, lcd.height()-4);
+    lcd.printf("%i", R.read());
+
+    lcd.locate(lcd.width()/5*4, lcd.height()-4);
+    lcd.printf("%i", X.read());
+
+    lcd.locate(0,0);
+    lcd.printf("current direction: %s", dirs[curr_dir]);
+}
+
 int main() {
+    ResetLeds();
+    DisplayIntLeds(val);
   while(1) {
+      lcd.cls();
+      if (L.read()) {curr_dir = 0;}
+      else if (R.read()) {curr_dir = 1;}
+      showInfoInputs();
+      wait_ms(10);
 
   }
 }
